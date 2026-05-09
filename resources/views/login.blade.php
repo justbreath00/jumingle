65a82e9a8d4c8adac65a812562a891f9527eda39
renierr <renierrtambogon@gmail.com>
2026-05-09 15:11:12 +0800

    mass commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <div>
+        <form action="" method="$_POST" autocomplete="off"> 
+            <label for="username" >username</label> <br>
+            <input type="text" name="username" value=""> 
+            <br>
+            <label for="password" >password</label> <br>
+            <input type="password" name="password" value=""> 
+            <br>
+            <input type="submit" name="submit" value="login">
+
+
+
+        </form>
+
+    </div>
+    
+</body>
+</html>
